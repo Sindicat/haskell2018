a43690b814db3d71044ee2d0e472dbcba56340f8
mmartynov <mmartynov@solanteq.com>
2018-11-01 22:01:02 +0300

    Fixed FP3 and FP7

--- haskell_intro_3.hs
+++ haskell_intro_3.hs
@@ -4,5 +4,6 @@ sumAndcount x | x < 0 = sumAndcount(-x)
               | otherwise = let
                     helper::Integer->Integer->Integer->(Integer,Integer)
                     helper summ count 0 = (summ, count)
-                    helper summ count n = helper (summ + (mod n 10)) (count + 1) (div n 10)
+                    helper summ count n = helper (summ + a) (count + 1) (b) where
+                      (b,a) = divMod n 10
                   in helper 0 0 x
--- haskell_intro_7.hs
+++ haskell_intro_7.hs
@@ -2,7 +2,7 @@ import Data.Char
 
 isPalindrome :: String -> Bool
 isPalindrome string = check (map toUpper string) True where
-	check string boolean | boolean == False || length string <= 1 = boolean
+	check string boolean | not boolean || length string <= 1 = boolean
 				               | otherwise = check (substring string) ((head string) == (last string))
 substring :: String -> String
 substring string = tail $ take ((length string)-1) string
